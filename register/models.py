d8ee041a08a936140361e1e70edb7f6faa1d8c65
DivyanshuSahu <dsahu1997@gmail.com>
2017-12-17 16:07:52 +0530

    table created

--- register/models.py
+++ register/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Users(models.Model) :
+	username = models.CharField(max_length=200, primary_key=True)
+	email = models.EmailField(max_length=200, unique=True)
+	password = models.CharField(max_length=250)
