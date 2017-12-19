2df03332849561f0af68a0ea1285dceb7aad3db4
DivyanshuSahu <dsahu1997@gmail.com>
2017-12-19 14:46:17 +0530

    login added with minor changes

--- minictf/urls.py
+++ minictf/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
 	path('register/', include('register.urls')),
+	path('login/', include('login.urls')),
     path('admin/', admin.site.urls)
 ]
