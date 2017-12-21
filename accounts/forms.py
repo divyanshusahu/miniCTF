fbc7ba21fc81929907b1f4247310abf15e8d0581
DivyanshuSahu <dsahu1997@gmail.com>
2017-12-21 14:36:50 +0530

    updated

--- accounts/forms.py
+++ accounts/forms.py
@@ -17,11 +17,11 @@ class LoginForm(AuthenticationForm) :
 	password = forms.CharField(max_length=250, min_length=8, label="", widget=forms.TextInput(attrs={'placeholder':'Password','class':'form-control','type':'password'}))
 
 class UpdateTeamDetails(forms.ModelForm) :
-	teamname = forms.CharField(max_length=250, label="", widget=forms.TextInput(attrs={'placeholder':'Team Name','class':'form-control'}))
-	email = forms.EmailField(max_length=250, label="", widget=forms.TextInput(attrs={'placeholder':'Team Email','class':'form-control', 'type':'email'}))
+	#teamname = forms.CharField(max_length=250, label="", widget=forms.TextInput(attrs={'placeholder':'Team Name','class':'form-control'}))
+	#email = forms.EmailField(max_length=250, label="", widget=forms.TextInput(attrs={'placeholder':'Team Email','class':'form-control', 'type':'email'}))
 	job = forms.CharField(max_length=250, label="", widget=forms.TextInput(attrs={'placeholder':'Job','class':'form-control'}))
 	company = forms.CharField(max_length=250, label="", widget=forms.TextInput(attrs={'placeholder':'Company','class':'form-control'}))
 
 	class Meta :
 		model = models.Teams
-		fields = ["teamname","email","job","company"]
+		fields = ["job","company"]
--- challenges/models.py
+++ challenges/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Challenges(models.Model) :
+	name = models.CharField(max_length=250, unique=True)
+	points = models.IntegerField()
+	category = models.CharField(max_length=100)
+	description = models.CharField(max_length=1000, null=True)
+	files = models.FileField(null=True)
+	flag = models.CharField(max_length=500)
+	author = models.CharField(max_length=250)
+	solved_by = models.IntegerField(null=True)
