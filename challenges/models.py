16aa87235c500798f1d5171d6c909c95ee1e5afc
DivyanshuSahu <dsahu1997@gmail.com>
2017-12-22 23:16:53 +0530

    challenges upload by admin completed

--- challenges/models.py
+++ challenges/models.py
@@ -2,12 +2,15 @@ from django.db import models
 
 # Create your models here.
 
+def get_upload_path(instance, filename) :
+	return 'uploads/challenges_{0}/{1}'.format(instance.name, filename)
+
 class Challenges(models.Model) :
 	name = models.CharField(max_length=250, unique=True, null=True)
 	category = models.CharField(max_length=100, null=True)
 	description = models.CharField(max_length=1000, blank=True, default="", null=True)
 	points = models.IntegerField(null=True)
-	files = models.FileField(null=True, blank=True, upload_to='uploads/')
+	files = models.FileField(null=True, blank=True, upload_to=get_upload_path)
 	flag = models.CharField(max_length=500, null=True)
 	author = models.CharField(max_length=250, null=True)
 	#solved_by = models.CharField()
