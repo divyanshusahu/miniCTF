d7e803c984189e3e1c2271c91f04d1824c08f4a8
DivyanshuSahu <dsahu1997@gmail.com>
2018-01-02 13:39:21 +0530

    thread submission successfull

--- forums/urls.py
+++ forums/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-	path('', views.index, name="index")
+	path('', views.index, name="index"),
+	re_path('(?P<pk>.+)', views.thread, name="thread")
 ]
--- forums/views.py
+++ forums/views.py
@@ -1,13 +1,38 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from . import forms
+from . import forms, models
+from accounts.models import Teams
+import datetime
 
 # Create your views here.
 
 @login_required(login_url="/accounts/login/")
 def index(request) :
+	success = 0
 	if request.method == 'POST' :
 		form = forms.AddThreadForm(request.POST, request.FILES)
+		if form.is_valid() :
+			success = 1
+			if request.FILES :
+				i = models.Threads(topic=request.POST['topic'],
+					posted_on=datetime.datetime.now(),
+					description=request.POST['description'],
+					posted_by=Teams.objects.get(teamname=request.user),
+					files=request.FILES['files'])
+				i.save()
+			else :
+				i = models.Threads(topic=request.POST['topic'],
+					posted_on=datetime.datetime.now(),
+					posted_by=Teams.objects.get(teamname=request.user),
+					description=request.POST['description'])
+				i.save()
 	else :
 		form = forms.AddThreadForm()
-	return render(request, 'index.html', {'form':form})
+
+	all_threads = models.Threads.objects.order_by("-posted_on")
+	print(all_threads)
+	return render(request, 'index.html', {'form':form,'success':success,'all_threads':all_threads})
+
+@login_required(login_url="/accounts/login/")
+def thread(request, pk) :
+	return render(request, 'index.html')
